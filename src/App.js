68198adaf5465f97a6823be5c649fb83ed8fde3d
emnalabiadh <emnalolita@gmail.com>
2021-02-23 09:29:53 +0100

    react-hooks

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import MovieApp from "./component/MovieApp";
+function App() {
+  return (
+    <div className="App">
+      <MovieApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/MovieData.js
@@ -0,0 +1,46 @@
+import movie1 from "../Assets/movie1.jpg";
+import movie2 from "../Assets/movie2.jpg";
+import movie3 from "../Assets/movie3.jpg";
+import movie4 from "../Assets/movie4.jpg";
+import movie5 from "../Assets/movie5.jpg";
+import movie6 from "../Assets/movie6.jpg";
+const moviesData = [
+  {
+    id: Math.random(),
+    title: "Holidate",
+    img: movie1,
+    rating: 3,
+  },
+  {
+    id: Math.random(),
+    title: "The Perfect Date",
+    img: movie2,
+    rating: 4,
+  },
+  {
+    id: Math.random(),
+    title: "To All The Boys ",
+    img: movie3,
+    rating: 4,
+  },
+  {
+    id: Math.random(),
+    title: "Adu",
+    img: movie4,
+    rating: 5,
+  },
+  {
+    id: Math.random(),
+    title: "Mirage",
+    img: movie5,
+    rating: 5,
+  },
+  {
+    id: Math.random(),
+    title: "The Silence",
+    img: movie6,
+    rating: 3,
+  },
+];
+
+export default moviesData;
